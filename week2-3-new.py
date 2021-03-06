6e49eeb419185d0396a7a2f5dae842ed7fe15f62
p83129 <77446871+p83129@users.noreply.github.com>
2021-03-06 18:12:17 +0800

    Add files via upload

---
+++ week2-3-new.py
@@ -0,0 +1,15 @@
+def maxProduct(nums):
+    max=0
+    temp=0    
+    
+    for i in range(len(nums)):
+        for j in range(len(nums)):            
+            if i!=j:
+                temp =nums[i]*nums[j]                
+                if temp>max:
+                    max=temp
+
+    print(max)  
+           
+maxProduct([5, 20, 2, 6])
+maxProduct([10, -20, 0, 3])
